d1988d75670745018d12c612c4f70545628dbbec
ALapina <larisa_42@mail.ru>
2020-09-17 14:21:50 +0300

    toggle fix

--- js/main.js
+++ js/main.js
@@ -2,27 +2,11 @@ const shareButton = document.querySelector('.share-button');
 const sharePanel = document.querySelector('.article-preview__share-panel');
 const shareArrow = shareButton.querySelector('path');
 
-console.log(shareArrow);
 
-let showSharePanel = false;
-
-
-function toogleSharePanel() {
-  if (!showSharePanel) {
-    //if panel is hidden then add class show-panel that make panel visible
-    sharePanel.classList.add('show-panel');
-    shareButton.classList.add('share-active');
-    shareArrow.classList.add('share-active');
-
-    showSharePanel = true;
-  } else {
-    //else if panel is visible then remove class show panel and make panel hidden by default
-    sharePanel.classList.remove('show-panel');
-    shareButton.classList.remove('share-active');
-    shareArrow.classList.remove('share-active');
-
-    showSharePanel = false;
-  }
+const toggleSharePanel = () => {
+  sharePanel.classList.toggle('show-panel');
+  shareButton.classList.toggle('share-active');
+  shareArrow.classList.toggle('share-active');
 }
 
-shareButton.addEventListener('click', toogleSharePanel);
+shareButton.addEventListener('click', toggleSharePanel);
